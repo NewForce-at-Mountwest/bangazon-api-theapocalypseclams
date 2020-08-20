471cb41255d56e7608b76515a9931a9f187589a6
Pat Shaver <pat.shaver@icloud.com>
2020-08-20 13:13:59 -0400

    finished PaymentTypeTests for GET ALL, Get one, PUT, POST and DELETE

--- TestBangazonAPI/DatabaseFixture.cs
+++ TestBangazonAPI/DatabaseFixture.cs
@@ -1,10 +1,74 @@
-﻿using System;
+﻿using BangazonAPI.Models;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace TestBangazonAPI
 {
-    class DatabaseFixture
+
+    public class DatabaseFixture : IDisposable
     {
+        private readonly string ConnectionString = @$"Server=localhost\SQLEXPRESS;Database=BangazonAPI;Trusted_Connection=True;";
+        public PaymentType AddDelTestType { get; set; }
+        public PaymentType EditType { get; set; }
+
+        public DatabaseFixture()
+        {
+            PaymentType newType = new PaymentType
+            {
+                AcctNumber = "9876543210",
+                Name = "TEST",
+                CustomerId = 2
+            };
+
+            PaymentType editType = new PaymentType
+            {
+                AcctNumber = "6789012345",
+                Name = "FOREDIT",
+                CustomerId = 2
+            };
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @$"INSERT INTO PaymentType (AcctNumber, Name, CustomerId)
+                                        OUTPUT INSERTED.Id
+                                        VALUES ('{newType.AcctNumber}', '{newType.Name}', '{newType.CustomerId}')";
+                    int newId = (int)cmd.ExecuteScalar();
+
+                    newType.Id = newId;
+
+                    AddDelTestType = newType;
+
+                    cmd.CommandText = @$"INSERT INTO PaymentType (AcctNumber, Name, CustomerId)
+                                        OUTPUT INSERTED.Id
+                                        VALUES ('{editType.AcctNumber}', '{editType.Name}', '{editType.CustomerId}')";
+                    int newEditId = (int)cmd.ExecuteScalar();
+
+                    editType.Id = newEditId;
+
+                    EditType = editType;
+
+                }
+            }
+        }
+        public void Dispose()
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @$"DELETE FROM PaymentType WHERE Name='TEST'";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
+
+
---
+++ TestBangazonAPI/PaymentTypeTest.cs
@@ -0,0 +1,130 @@
+﻿using BangazonAPI.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestBangazonAPI
+{
+    [Collection("Database collection")]
+    public class PaymentTypeTest : IClassFixture<DatabaseFixture>
+    {
+        DatabaseFixture fixture;
+        public PaymentTypeTest(DatabaseFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+   
+        [Fact]
+        public async Task Test_Get_All_PaymentTypes()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                // Arrange
+                var response = await client.GetAsync("/api/PaymentType");
+
+                // Act
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var typeList = JsonConvert.DeserializeObject<List<PaymentType>>(responseBody);
+
+                // Assert
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(typeList.Count > 0);
+            }
+        }
+        [Fact]
+        public async Task Create_One_PaymentType()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                // Arrange
+                PaymentType newType = new PaymentType()
+                {
+                    AcctNumber = "5432109876",
+                    Name = "NewPayType",
+                    CustomerId = 2
+                };
+                string jsonCoffee = JsonConvert.SerializeObject(newType);
+
+                // Act
+                HttpResponseMessage response = await client.PostAsync("/api/PaymentType",
+                    new StringContent(jsonCoffee, Encoding.UTF8, "application/json"));
+                string responseBody = await response.Content.ReadAsStringAsync();
+                PaymentType paymentTypeResponse = JsonConvert.DeserializeObject<PaymentType>(responseBody);
+
+                // Assert
+                Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+                Assert.Equal(paymentTypeResponse.Name, newType.Name);
+            }
+        }
+
+ 
+        [Fact]
+        public async Task Test_One_PaymentType()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+
+                var response = await client.GetAsync($"/api/paymenttype/{fixture.AddDelTestType.Id}");
+                string responseBody = await response.Content.ReadAsStringAsync();
+                PaymentType singleType = JsonConvert.DeserializeObject<PaymentType>(responseBody);
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                //Assert.Equal(fixture.TestCoffee.Title, singleCoffee.Title);
+            }
+        }
+        [Fact]
+        public async Task Edit_PaymentType()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                // Arrange
+                PaymentType editedType = new PaymentType()
+                {
+                    AcctNumber = "6789012345",
+                    Name = "EDITED PaymentType",
+                    CustomerId = 2
+                };
+                // Act
+                string jsonCoffee = JsonConvert.SerializeObject(editedType);
+                HttpResponseMessage response = await client.PutAsync($"/api/paymenttype/{fixture.EditType.Id}",
+                    new StringContent(jsonCoffee, Encoding.UTF8, "application/json"));
+                // Assert
+                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+                /*
+            GET section
+            Verify that the PUT operation was successful
+        */
+                var getEditedType = await client.GetAsync($"/api/paymenttype/{fixture.EditType.Id}");
+                getEditedType.EnsureSuccessStatusCode();
+
+                string getEditedTypeBody = await getEditedType.Content.ReadAsStringAsync();
+                PaymentType editType = JsonConvert.DeserializeObject<PaymentType>(getEditedTypeBody);
+
+                Assert.Equal(HttpStatusCode.OK, getEditedType.StatusCode);
+                Assert.Equal(editedType.Name, editType.Name);
+
+            }
+        }
+
+    
+        [Fact]
+        public async Task Delete_PaymentType()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                HttpResponseMessage response = await client.DeleteAsync($"/api/paymentType/{fixture.AddDelTestType.Id}");
+                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+            }
+        }
+
+     }
+}
+
